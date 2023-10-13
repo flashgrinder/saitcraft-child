ae45b5b08c0436737dcba8ca4ccaf54273138804
flashgrinder <flashgrinder92@gmail.com>
2023-10-13 12:55:16 +0400

    make form phone

--- page-beta.php
+++ page-beta.php
@@ -15,4 +15,22 @@
 		</div>
 	</section>
 
+	<!-- Form Phone -->
+	<section class="form-phone">
+		<div class="form-phone__body container">
+			<div class="form-phone__outer">
+				<div class="form-phone__info">
+					<h3 class="form-phone__heading title title--pre-large title--dark title--w-semi-bold center">
+						Помочь с выбором
+					</h3>
+					<p class="form-phone__subtitle text text--large text--dark text--w-semi-bold center">
+						Оставьте номер и мы перезвоним вам в течение 1 часа
+					</p>
+				</div>
+				<?= do_shortcode('[contact-form-7 id="502f623" title="Контактная форма c телефоном" html_class="form-phone__form"]'); ?>
+			</div>
+		</div>
+	</section>
+	<!-- /. Form Phone -->
+
 <?php get_footer(); ?>
