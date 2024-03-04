6d58c15dcc8ef91009e0c6ab87913fb8f2b75a3c
nik1224 <0lezgin5@gmail.com>
2024-03-04 11:29:36 +0500

    adding a script to the footer

--- footer.php
+++ footer.php
@@ -326,6 +326,39 @@
 </div>
 <!-- /. Success -->
 
+<script>
+	(function(ratingRuneta) {
+		var rrcid = new URLSearchParams(window.location.search).get(ratingRuneta);
+		if (rrcid) window.sessionStorage.setItem(ratingRuneta, rrcid);
+		rrcid = window.sessionStorage.getItem(ratingRuneta);
+		(function(m, e, t, r, i, k, a) {
+			m[i] = m[i] || function() {
+				(m[i].a = m[i].a || []).push(arguments)
+			};
+			m[i].l = 1 * new Date();
+			for (var j = 0; j < document.scripts.length; j++) {
+				if (document.scripts[j].src === r) {
+					return;
+				}
+			}
+			k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+		})
+		(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+		ym(92096748, "init", {
+			'clickmap': false,
+			'trackLinks': false,
+			'accurateTrackBounce': true,
+			'webvisor': false,
+			'params': {
+				'rrcid': rrcid
+			}
+
+
+		});
+	})('rrcid');
+</script>
+
+
 <?php wp_footer(); ?>
 </body>
 
