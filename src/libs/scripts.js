6c31fa9f3dcfb357e701c402a68e5b67c805c313
flashgrinder <flashgrinder92@gmail.com>
2024-03-17 16:25:53 +0400

    make tmpl blocks for theme wp

--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -2,6 +2,7 @@ import gsap from 'gsap';
 import modalForm from '../libs/modules/modal.js';
 import phoneMask from '../libs/modules/phone-mask';
 import fancybox from '../libs/modules/fancybox';
+import cases from '../libs/modules/cases';
 
 document.addEventListener('DOMContentLoaded', function(e) {
 
@@ -10,8 +11,11 @@ document.addEventListener('DOMContentLoaded', function(e) {
         // force3D: true
     });
 
+    const casesTrue = document.querySelector('.js-cases');
+
     modalForm.init();
     phoneMask.init();
     fancybox.init();
+    casesTrue ? cases.init() : false;
 
 });
