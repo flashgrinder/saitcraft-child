30a0e9c9349977d7a746161585f003405aab5c72
nik12224 <0lezgin5@gmail.com>
2024-07-04 17:06:11 +0500

    disabled GTM

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
     ?>
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQTS4MC" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQTS4MC" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
     <!-- End Google Tag Manager (noscript) -->
 
     <!-- Yandex.Metrika counter -->
