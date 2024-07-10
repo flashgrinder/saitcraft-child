35c31400f1a0295e03566a8c8e30419c243dde0e
flashgrinder <flashgrinder92@gmail.com>
2024-07-10 10:04:09 +0400

    test ya iframe 7

--- inc/actions.php
+++ inc/actions.php
@@ -1,5 +1,5 @@
 <?php
-const VERSION_THEME_FILES = '1.0.7.4';
+const VERSION_THEME_FILES = '1.0.7.5';
 
 add_action('wp_enqueue_scripts', function () {
 
@@ -44,25 +44,6 @@ add_action('wp_head', function () {
 ## JS в конце документа
 add_action('wp_footer', function () {
 ?>
-
-	<!-- Yandex.Metrika counter -->
-<!--	<script type="text/javascript" >-->
-<!--        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};-->
-<!--            m[i].l=1*new Date();-->
-<!--            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}-->
-<!--            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})-->
-<!--        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");-->
-<!---->
-<!---->
-<!--        ym(25803377, "init", {-->
-<!--            clickmap:true,-->
-<!--            trackLinks:true,-->
-<!--            accurateTrackBounce:true,-->
-<!--            webvisor:true-->
-<!--        });-->
-<!--	</script>-->
-<!--	<noscript><div><img src="https://mc.yandex.ru/watch/25803377" style="position:absolute; left:-9999px;" alt="" /></div></noscript>-->
-	<!-- /Yandex.Metrika counter -->
 
 	<script>
 		// (function(ratingRuneta) {
