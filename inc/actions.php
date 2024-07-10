8608d57662dd958cdcecdaedc19026c1fe0243da
flashgrinder <flashgrinder92@gmail.com>
2024-07-10 09:38:38 +0400

    test ya iframe 2

--- inc/actions.php
+++ inc/actions.php
@@ -1,5 +1,5 @@
 <?php
-const VERSION_THEME_FILES = '1.0.7';
+const VERSION_THEME_FILES = '1.0.7.1';
 
 add_action('wp_enqueue_scripts', function () {
 
@@ -67,35 +67,35 @@ add_action('wp_footer', function () {
 ?>
 
 	<script>
-		(function(ratingRuneta) {
-			var rrcid = new URLSearchParams(window.location.search).get(ratingRuneta);
-			if (rrcid) window.sessionStorage.setItem(ratingRuneta, rrcid);
-			rrcid = window.sessionStorage.getItem(ratingRuneta);
-			(function(m, e, t, r, i, k, a) {
-				m[i] = m[i] || function() {
-					(m[i].a = m[i].a || []).push(arguments)
-				};
-				m[i].l = 1 * new Date();
-				for (var j = 0; j < document.scripts.length; j++) {
-					if (document.scripts[j].src === r) {
-						return;
-					}
-				}
-				k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-			})
-			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-			ym(25803377, "init", {
-				'clickmap': false,
-				'trackLinks': false,
-				'accurateTrackBounce': true,
-				'webvisor': false,
-				'params': {
-					'rrcid': rrcid
-				}
-
-
-			});
-		})('rrcid');
+		// (function(ratingRuneta) {
+		// 	var rrcid = new URLSearchParams(window.location.search).get(ratingRuneta);
+		// 	if (rrcid) window.sessionStorage.setItem(ratingRuneta, rrcid);
+		// 	rrcid = window.sessionStorage.getItem(ratingRuneta);
+		// 	(function(m, e, t, r, i, k, a) {
+		// 		m[i] = m[i] || function() {
+		// 			(m[i].a = m[i].a || []).push(arguments)
+		// 		};
+		// 		m[i].l = 1 * new Date();
+		// 		for (var j = 0; j < document.scripts.length; j++) {
+		// 			if (document.scripts[j].src === r) {
+		// 				return;
+		// 			}
+		// 		}
+		// 		k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+		// 	})
+		// 	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+		// 	ym(25803377, "init", {
+		// 		'clickmap': false,
+		// 		'trackLinks': false,
+		// 		'accurateTrackBounce': true,
+		// 		'webvisor': false,
+		// 		'params': {
+		// 			'rrcid': rrcid
+		// 		}
+		//
+		//
+		// 	});
+		// })('rrcid');
 	</script>
 
 	<script>
