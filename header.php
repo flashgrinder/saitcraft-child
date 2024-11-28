75f18b8d4760d6703b99c11bdb2404145df0a1b3
flashgrinder <flashgrinder92@gmail.com>
2024-11-28 15:00:57 +0400

    add gtm

--- header.php
+++ header.php
@@ -13,6 +13,11 @@
 </head>
 
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQTS4MC" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
     <?php wp_body_open(); ?>
     <?php
     $categories = get_the_category();
